79092c683716fd1e7c3201bd4888f4e735188801
Karolina Balszan <86183388+balszan@users.noreply.github.com>
2025-04-05 09:20:57 +0100

    Added full form validation

--- main.js
+++ main.js
@@ -18,6 +18,64 @@ function validateInputs() {
   const passwordValue = password.value.trim()
   const confirmPasswordValue = confirmPassword.value.trim()
   const sourceValue = source.value.trim()
+
+  // Username validation
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be empty")
+  } else {
+    setSuccessFor(username)
+  }
+
+  // Email validation
+  if (emailValue === "") {
+    setErrorFor(email, "Email field cannot be empty")
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Please enter a valid email")
+  } else {
+    setSuccessFor(email)
+  }
+
+  // Confirm email validation
+  if (confirmEmailValue === "") {
+    setErrorFor(confirmEmail, "Email field cannot be empty")
+  } else if (!isEmail(confirmEmailValue)) {
+    setErrorFor(confirmEmail, "Please enter a valid email")
+  } else if (confirmEmailValue !== emailValue) {
+    setErrorFor(confirmEmail, "The emails don't match")
+  } else {
+    setSuccessFor(confirmEmail)
+  }
+
+  // Password validation
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Please enter password")
+  } else if (!isValidPassword(passwordValue)) {
+    setErrorFor(
+      password,
+      "Password has to have at least 8 leters and a special character"
+    )
+  } else {
+    setSuccessFor(password)
+  }
+
+  // Confirm password validation
+
+  if (confirmPasswordValue === "") {
+    setErrorFor(confirmPassword, "Please confirm password")
+  } else if (confirmPasswordValue !== passwordValue) {
+    setErrorFor(confirmPassword, "The passwords don't match")
+  } else {
+    setSuccessFor(confirmPassword)
+  }
+
+  // Source validation
+
+  if (sourceValue === "") {
+    setErrorFor(source, "Please select an option")
+  } else {
+    setSuccessFor(source)
+  }
 }
 
 function setErrorFor(input, message) {
@@ -30,4 +88,14 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
   const formControl = input.parentElement
   formControl.className = "form-control success"
+}
+
+function isEmail(email) {
+  return /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(email)
+}
+
+function isValidPassword(password) {
+  return /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-zA-Z0-9]).{8,}$/.test(
+    password
+  )
 }
